10b259266968e7b0ed93430620508554365d26e4
aymanalareqi <77433449+aymanalareqi@users.noreply.github.com>
2025-09-15 22:41:19 +0000

    Fix styling

--- src/WasenderExtendServiceProvider.php
+++ src/WasenderExtendServiceProvider.php
@@ -4,7 +4,6 @@ namespace Alareqi\WasenderExtend;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Alareqi\WasenderExtend\Commands\WasenderExtendCommand;
 
 class WasenderExtendServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Alareqi\WasenderExtend\Tests;
 
+use Alareqi\WasenderExtend\WasenderExtendServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Alareqi\WasenderExtend\WasenderExtendServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn(string $modelName) => 'Alareqi\\WasenderExtend\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'Alareqi\\WasenderExtend\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
