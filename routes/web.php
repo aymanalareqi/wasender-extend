5adb376507cf012ef24a06edcd80a830276d6d46
aymanalareqi <77433449+aymanalareqi@users.noreply.github.com>
2025-09-16 20:14:04 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Alareqi\WasenderExtend\Http\Controllers\User\AppsController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'user', 'as' => 'user.', 'middleware' => ['auth', 'user', 'saas','web']], function () {
+Route::group(['prefix' => 'user', 'as' => 'user.', 'middleware' => ['auth', 'user', 'saas', 'web']], function () {
     Route::get('apps/', [AppsController::class, 'index'])->name('apps.index');
 });
